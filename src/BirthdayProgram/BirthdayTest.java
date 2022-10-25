9d7344393226170f67d476e0c4ba08b31e7adc7c
santino732 <79545456+santino732@users.noreply.github.com>
2022-10-25 11:56:15 -0400

    Add files via upload

---
+++ src/BirthdayProgram/BirthdayTest.java
@@ -0,0 +1,19 @@
+package BirthdayProgram;
+
+import java.text.DecimalFormat;
+
+public class BirthdayTest {
+	
+	public static void main(String[] args) {
+		
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		//assigns prob variable to the probability of N amount of people 
+		double prob = Birthday.birthday(0);
+	    System.out.print("Probability of N number of people sharing a birthday is " + df.format(prob*100) + "%");
+	    
+	    //# of runs set to 100000 # of students set to 25, probability ranges from 55-58%
+
+	}
+
+}
---
+++ src/MonteHallProgram/MonteHall.java
@@ -0,0 +1,50 @@
+package MonteHallProgram;
+
+public class MonteHall {
+	
+	public static double SwitchDoor(double prob) {
+        int trials = 10000;
+        int wins = 0;
+        for (int i = 0; i < trials; i++) {
+            int prize  = (int) (3 * Math.random());   //3 being the number of doors.
+            int choice = (int) (3 * Math.random());
+            int reveal;
+            
+            do {
+                reveal = (int) (3 * Math.random());
+            } while ((reveal == choice) || (reveal == prize));
+
+           int other = 0 + 1 + 2 - reveal - choice;  
+           
+            if (other == prize) wins++;      //other is other door being selected 
+        } 
+        
+        prob = ((1.0 * wins / trials) * 100);
+
+        return prob;
+    }
+	
+	//the only difference between switching doors and not switching doors is
+	//not subtracting change from int other. This drops the probability of winning.
+	public static double DontSwitchDoor(double prob2) {
+	       int trials = 10000;
+	       int wins = 0;
+
+	       for (int i = 0; i < trials; i++) {
+	           int prize  = (int) (3 * Math.random());
+	           int choice = (int) (3 * Math.random());
+
+	           int reveal;
+	           do {
+	               reveal = (int) (3 * Math.random());
+	           } while ((reveal == choice) || (reveal == prize));
+
+	          int other = 0 + 1 + 2 - reveal;    //only change because we are not changing door
+
+	           if (other == prize) wins++;
+	       } 
+	       prob2 = ((1.0 * wins / trials) * 100);
+	       
+	       return prob2;
+	   }
+}
